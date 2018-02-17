0d0b287acd6212c7ac25a77b171eb0154f9fe752
Michal Cwienczek <michael.cwienczek@gmail.com>
2018-02-17 10:13:02 +0100

    Added more comments to the script

--- apply_cluster.sh
+++ apply_cluster.sh
@@ -1,4 +1,12 @@
+# Apply kubectl/yaml files first
 kubectl apply --recursive --prune --all -f kubectl 
+
+# Initialize helm and wait for tiller to be ready
 helm init --service-account tiller;kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system;
+
+# Update helm repo
 helm repo update
+
+# Apply all yaml files in folder landscaper
+# NOTE! This setup won't work with nested folders as landscaper only applies top-level yaml files
 landscaper apply landscaper
